cc3870ea30f144a8d3c1dbcc600a556b8283c7ec
이진석 <gkrryrwkk6@naver.com>
2024-05-31 17:15:39 +0900

    셀렉 프롬 웨얼

--- test.ts
+++ test.ts
@@ -7,6 +7,16 @@ async function main() {
             name: true,
             age: true,
         }
+    })  //  강의기준
+    console.log(result)
+}
+
+async function selectColum() {
+    let result = await prisma.user.findMany({
+        select:{
+            name: true,
+            age: true,
+        }
     })  // 셀렉트에서 name과 age 만 가져옴
     console.log(result)
 }
@@ -19,5 +29,15 @@ async function selectAll() {
     console.log(result2)
 }
 
-main()
+async function selectWhere() {
+    let result2 = await prisma.user.findMany({
+    where : {age:20}
+    })
+    // 유저 테이블에서 findMany() 조건에 맞는거 모두 가져옴 셀렉트
+    console.log(result2)
+}
+
+
+selectColum()
 selectAll()
+selectWhere()
