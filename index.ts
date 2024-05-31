59605678c312b1ee15321e15c87021589516af51
이진석 <gkrryrwkk6@naver.com>
2024-05-31 16:23:58 +0900

    프리즈마 실행

---
+++ index.ts
@@ -0,0 +1,25 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
+
+async function main() {
+    let result = await prisma.user.findMany({
+        select:{
+            name: true,
+            age: true,
+        }
+    })
+ // 셀렉트에서 name과 age 만 가져옴
+    console.log(result)
+}
+
+
+async function selectAll() {
+    let result2 = await prisma.user.findMany({
+ 
+    })
+    // 유저 테이블에서 findMany() 조건에 맞는거 모두 가져옴 셀렉트
+    console.log(result2)
+}
+
+main()
+selectAll()
